4b76a7c59024efae34525707cfedcbcdefc51867
Saad Hassan <msaadhassan99@gmail.com>
2020-07-16 07:49:57 -0700

    Implementation for historical data

--- session 2/src/historical/index.js
+++ session 2/src/historical/index.js
@@ -1,18 +1,23 @@
 const CoinbasePro = require('coinbase-pro')
 
 class HistoricalService {
-    constructor({ start, end, interval, product }) {
+    constructor({ start, end, interval = 300, product }) {
         this.publicClient = new CoinbasePro.PublicClient()
         this.start = start
         this.end = end
         this.interval = interval
+        this.product = product
     }
 
     async getData() {
         const results = await this.publicClient.getProductHistoricRates(this.product, {
-            granularity: this.interval
+            start: this.start,
+            end: this.end,
+            granularity: 300
         })
 
         return results
     }
 }
+
+module.exports = HistoricalService
